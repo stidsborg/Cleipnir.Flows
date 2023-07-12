ee4decb8c3028c4e5bbfd63871eed5e40124ee31
stidsborg <stidsborg@cleipnir.net>
2023-07-12 15:48:13 +0200

    Added AzureBlobFlowStore and updated other project to use ResilientFunctions 1.0.6

---
+++ Cleipnir.Flows.AzureBlob/AzureBlobFlowStore.cs
@@ -0,0 +1,8 @@
+﻿using Cleipnir.ResilientFunctions.AzureBlob;
+
+namespace Cleipnir.Flows.AzureBlob;
+
+public class AzureBlobFlowStore : AzureBlobFunctionStore, IFlowStore
+{
+    public AzureBlobFlowStore(string connectionString, string tablePrefix = "") : base(connectionString, tablePrefix) { }
+}
