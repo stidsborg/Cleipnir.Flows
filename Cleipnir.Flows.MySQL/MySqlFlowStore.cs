a2ee6fac6d66cd664205e75ca015348dc9f18a34
stidsborg <stidsborg@cleipnir.net>
2023-07-12 15:02:06 +0200

    Added MySQL FlowStore

---
+++ Cleipnir.Flows.MySQL/MySqlFlowStore.cs
@@ -0,0 +1,8 @@
+﻿using Cleipnir.ResilientFunctions.MySQL;
+
+namespace Cleipnir.Flows.MySQL;
+
+public class MySqlFlowStore : MySqlFunctionStore, IFlowStore
+{
+    public MySqlFlowStore(string connectionString, string tablePrefix = "") : base(connectionString, tablePrefix) { }
+}
