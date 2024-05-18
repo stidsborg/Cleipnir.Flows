8762071fc8079675e4d8d6d8adf6892dcdf78fd6
stidsborg <stidsborg@cleipnir.net>
2024-05-18 08:40:53 +0200

    Updated Cleipnir.ResilientFunctions to the latest commit

---
+++ Cleipnir.Flows.Tests/Flows/ParamlessFlowsTests.cs
@@ -0,0 +1,86 @@
+﻿using Cleipnir.ResilientFunctions.Domain;
+using Cleipnir.ResilientFunctions.Reactive.Extensions;
+using Cleipnir.ResilientFunctions.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Cleipnir.Flows.Tests.Flows;
+
+[TestClass]
+public class UnitFlowsTests
+{
+    [TestMethod]
+    public async Task SimpleFlowCompletesSuccessfully()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddTransient<SimpleUnitFlow>();
+
+        var flowStore = new InMemoryFunctionStore();
+        var flowsContainer = new FlowsContainer(
+            flowStore,
+            serviceCollection.BuildServiceProvider()
+        );
+
+        var flows = flowsContainer.CreateFlows<SimpleUnitFlow, string>(nameof(SimpleUnitFlow));
+        await flows.Run("someInstanceId", "someParameter");
+        
+        SimpleUnitFlow.InstanceId.ShouldBe("someInstanceId");
+        SimpleUnitFlow.ExecutedWithParameter.ShouldBe("someParameter");
+
+        var controlPanel = await flows.ControlPanel(instanceId: "someInstanceId");
+        controlPanel.ShouldNotBeNull();
+        controlPanel.Status.ShouldBe(Status.Succeeded);
+    }
+
+    public class SimpleUnitFlow : Flow<string>
+    {
+        public static string? ExecutedWithParameter { get; set; }
+        public static string? InstanceId { get; set; } 
+
+        public override async Task Run(string param)
+        {
+            await Task.Delay(1);
+            ExecutedWithParameter = param;
+            InstanceId = Workflow.FunctionId.InstanceId.ToString();
+        }
+    }
+    
+    [TestMethod]
+    public async Task EventDrivenFlowCompletesSuccessfully()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddTransient<EventDrivenUnitFlow>();
+
+        var flowStore = new InMemoryFunctionStore();
+        var flowsContainer = new FlowsContainer(
+            flowStore,
+            serviceCollection.BuildServiceProvider()
+        );
+
+        var flows = flowsContainer.CreateFlows<EventDrivenUnitFlow, string>(nameof(EventDrivenUnitFlow));
+
+        await flows.Schedule("someInstanceId", "someParameter");
+
+        await Task.Delay(10);
+        var controlPanel = await flows.ControlPanel(instanceId: "someInstanceId");
+        controlPanel.ShouldNotBeNull();
+        controlPanel.Status.ShouldBe(Status.Executing);
+        
+        var eventSourceWriter = flows.MessageWriter("someInstanceId");
+        await eventSourceWriter.AppendMessage(2);
+
+        await controlPanel.WaitForCompletion();
+
+        await controlPanel.Refresh();
+        controlPanel.ShouldNotBeNull();
+        controlPanel.Status.ShouldBe(Status.Succeeded);
+    }
+    
+    public class EventDrivenUnitFlow : Flow<string>
+    {
+        public override async Task Run(string param)
+        {
+            await Messages.FirstOfType<int>();
+        }
+    }
+}
