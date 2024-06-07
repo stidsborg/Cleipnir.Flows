8dd0792eb30cb42e9e3f8d5b708f1fd714fdb25f
stidsborg <stidsborg@cleipnir.net>
2024-06-07 10:32:21 +0200

    Dry'ed Flows and added SubscribeToFlowTests

--- Cleipnir.Flows.Tests/Flows/SubscribeToFlowTests.cs
+++ Cleipnir.Flows.Tests/Flows/SubscribeToFlowTests.cs
@@ -1,7 +1,88 @@
+using Cleipnir.ResilientFunctions.Domain;
+using Cleipnir.ResilientFunctions.Helpers;
+using Cleipnir.ResilientFunctions.Reactive.Extensions;
+using Cleipnir.ResilientFunctions.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
 namespace Cleipnir.Flows.Tests.Flows;
 
 [TestClass]
 public class SubscribeToFlowTests
 {
+    [TestMethod]
+    public async Task MessageIsRoutedToFlowUsingInstanceId()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddTransient<RouteToInstanceParamlessFlow>();
+
+        var flowStore = new InMemoryFunctionStore();
+        var flowsContainer = new FlowsContainer(
+            flowStore,
+            serviceCollection.BuildServiceProvider()
+        );
+
+        var flows = new RouteToInstanceParamlessFlows(flowsContainer);
+        await flows.Schedule("SomeInstanceId");
+        
+        var msg = new Message("SomeInstanceId", "SomeValue");
+        await flowsContainer.DeliverMessage(msg);
+
+        await BusyWait.UntilAsync(() => RouteToInstanceParamlessFlow.ReceivedMessage != null);
+        RouteToInstanceParamlessFlow.ReceivedMessage.ShouldBe(msg);
+    }
+
+    public class RouteToInstanceParamlessFlow : Flow, ISubscribeTo<Message>
+    {
+        public static RoutingInfo Correlate(Message msg) => Route.To(msg.Route);
+        
+        public static Message? ReceivedMessage { get; set; }
+
+        public override async Task Run()
+        {
+            ReceivedMessage = await Messages.FirstOfType<Message>();
+        }
+    }
+
+    [TestMethod]
+    public async Task MessageIsRoutedToFlowUsingCorrelationId()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddTransient<RouteUsingCorrelationParamlessFlow>();
+
+        var flowStore = new InMemoryFunctionStore();
+        var flowsContainer = new FlowsContainer(
+            flowStore,
+            serviceCollection.BuildServiceProvider()
+        );
+
+        var flows = new RouteUsingCorrelationParamlessFlows(flowsContainer);
+        await flows.Schedule("SomeInstanceId");
+
+        await BusyWait.UntilAsync(() => RouteUsingCorrelationParamlessFlow.CorrelationRegistered);
+        
+        var msg = new Message("SomeCorrelationId", "SomeValue");
+        await flowsContainer.DeliverMessage(msg);
+
+        await BusyWait.UntilAsync(() => RouteUsingCorrelationParamlessFlow.ReceivedMessage != null);
+        RouteUsingCorrelationParamlessFlow.ReceivedMessage.ShouldBe(msg);
+    }
+
+    public class RouteUsingCorrelationParamlessFlow : Flow, ISubscribeTo<Message>
+    {
+        public static RoutingInfo Correlate(Message msg) => Route.Using(msg.Route);
+
+        public static volatile bool CorrelationRegistered;
+        public static Message? ReceivedMessage { get; set; }
+
+        public override async Task Run()
+        {
+            await Workflow.RegisterCorrelation("SomeCorrelationId");
+            CorrelationRegistered = true;
+            
+            ReceivedMessage = await Messages.FirstOfType<Message>();
+        }
+    }
     
+    public record Message(string Route, string Value);
 }
--- Cleipnir.Flows/Flows.cs
+++ Cleipnir.Flows/Flows.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 using Cleipnir.Flows.CrossCutting;
 using Cleipnir.ResilientFunctions;
@@ -12,90 +14,45 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Cleipnir.Flows;
 
-public class Flows<TFlow> where TFlow : Flow
+public abstract class BaseFlows<TFlow> where TFlow : notnull
 {
-    private readonly FlowsContainer _flowsContainer;
-    private readonly ParamlessRegistration _registration;
-
-    private readonly Next<TFlow, Unit, Unit> _next;
-    private readonly Action<TFlow, Workflow> _workflowSetter;
-    private readonly Action<TFlow, States> _stateSetter; 
+    private FlowsContainer FlowsContainer { get; }
     
-    public Flows(string flowName, FlowsContainer flowsContainer)
-    {
-        _flowsContainer = flowsContainer;
-        _workflowSetter = CreateWorkflowSetter();
-        _stateSetter = CreateStateSetter();
-        
-        _next = CreateCallChain(flowsContainer.ServiceProvider);
+    protected BaseFlows(FlowsContainer flowsContainer) => FlowsContainer = flowsContainer;
 
-        var subscriptions = typeof(TFlow)
-            .GetInterfaces()
-            .Select(i => i.IsGenericType
-                ? new { GenericType = i, OpenGenericType = i.GetGenericTypeDefinition() }
-                : null
-            )
-            .Where(a => a is not null && a.OpenGenericType == typeof(ISubscribeTo<>))
-            .Select(a => new { a!.GenericType, SubscriptionType = a.GenericType.GetGenericArguments()[0] })
-            //.Select(a => new RoutingInformation())
-            .ToList();
-        
-        _registration = flowsContainer.FunctionRegistry.RegisterParamless(
-            flowName,
-            PrepareAndRunFlow
-            /*new Settings(
-                routes: subscriptions.Any()
-                    ?
-                )*/
+    private static Action<TFlow, Workflow> CreateWorkflowSetter()
+    {
+        ParameterExpression flowParam = Expression.Parameter(typeof(TFlow), "flow");
+        ParameterExpression contextParam = Expression.Parameter(typeof(Workflow), "workflow");
+        MemberExpression propertyExpr = Expression.Property(flowParam, nameof(Flow.Workflow));
+
+        BinaryExpression assignExpr = Expression.Assign(propertyExpr, contextParam);
+
+        // Create a lambda expression
+        Expression<Action<TFlow, Workflow>> lambdaExpr = Expression.Lambda<Action<TFlow, Workflow>>(
+            assignExpr,
+            flowParam,
+            contextParam
         );
-        
-        
+
+        // Compile and invoke the lambda expression
+        var setter = lambdaExpr.Compile();
+        return setter;
     }
 
-    private Next<TFlow, Unit, Unit> CreateCallChain(IServiceProvider serviceProvider)
-    {
-        return CallChain.Create<TFlow, Unit, Unit>(
-            _flowsContainer.Middlewares,
-            async (_, workflow) =>
+    protected static IReadOnlyList<RoutingInformation> CreateRoutingInformation() =>
+        typeof(TFlow)
+            .GetMethods(BindingFlags.Public | BindingFlags.Static)
+            .Where(m => m.Name is "Correlate" && m.GetParameters().Length == 1 && m.ReturnType == typeof(RoutingInfo))
+            .Select(m => new
             {
-                await using var scope = serviceProvider.CreateAsyncScope();
+                ParamType = m.GetParameters()[0].ParameterType,
+                Resolver = new RouteResolver(msg => (RoutingInfo)m.Invoke(obj: null, [msg])!)
+            })
+            .Select(a => new RoutingInformation(a.ParamType, a.Resolver))
+            .ToList();
 
-                var flow = scope.ServiceProvider.GetRequiredService<TFlow>();
-                _workflowSetter(flow, workflow);
-                _stateSetter(flow, workflow.States);
-                                
-                await flow.Run();
-                
-                return Unit.Instance;
-            }
-        );
-    }
-
-    public async Task<ControlPanel?> ControlPanel(string instanceId)
-    {
-        var controlPanel = await _registration.ControlPanel(instanceId);
-        return controlPanel;
-    }
-    
-    protected Task<TState?> GetState<TState>(string functionInstanceId) where TState : WorkflowState, new() 
-        => _registration.GetState<TState>(functionInstanceId);
-    
-    public MessageWriter MessageWriter(string instanceId) 
-        => _registration.MessageWriters.For(instanceId);
-
-    public Task Run(string instanceId) 
-        => _registration.Invoke(instanceId);
-
-    public Task Schedule(string instanceId)
-        => _registration.Schedule(instanceId);
-    
-    public Task ScheduleAt(string instanceId, DateTime delayUntil) => _registration.ScheduleAt(instanceId, delayUntil);
-    public Task ScheduleIn(string functionInstanceId, TimeSpan delay) => _registration.ScheduleIn(functionInstanceId, delay);
-    
-    private async Task<Result<Unit>> PrepareAndRunFlow(Workflow workflow) 
-        => await _next(Unit.Instance, workflow);
-    
-    private Action<TFlow, States> CreateStateSetter()
+    private static Action<TFlow, States> CreateStateSetter()
     {
         var iHaveStateType = typeof(TFlow)
             .GetInterfaces()
@@ -121,68 +78,87 @@ public class Flows<TFlow> where TFlow : Flow
         };
     }
     
-    private Action<TFlow, Workflow> CreateWorkflowSetter()
+    protected Next<TFlow, TParam, TResult> CreateMiddlewareCallChain<TParam, TResult>(Func<TFlow, TParam, Task<TResult>> runFlow) where TParam : notnull
     {
-        ParameterExpression flowParam = Expression.Parameter(typeof(TFlow), "flow");
-        ParameterExpression contextParam = Expression.Parameter(typeof(Workflow), "workflow");
-        MemberExpression propertyExpr = Expression.Property(flowParam, nameof(Flow.Workflow));
-                
-        BinaryExpression assignExpr = Expression.Assign(propertyExpr, contextParam);
-
-        // Create a lambda expression
-        Expression<Action<TFlow, Workflow>> lambdaExpr = Expression.Lambda<Action<TFlow, Workflow>>(
-            assignExpr,
-            flowParam,
-            contextParam
-        );
-
-        // Compile and invoke the lambda expression
-        var setter = lambdaExpr.Compile();
-        return setter;
-    }
-}
-
-public class Flows<TFlow, TParam> 
-    where TFlow : Flow<TParam>
-    where TParam : notnull
-{
-    private readonly FlowsContainer _flowsContainer;
-    private readonly FuncRegistration<TParam, Unit> _registration;
-
-    private readonly Next<TFlow, TParam, Unit> _next;
-    private readonly Action<TFlow, Workflow> _workflowSetter;
-    private readonly Action<TFlow, States> _stateSetter; 
-    
-    public Flows(string flowName, FlowsContainer flowsContainer)
-    {
-        _flowsContainer = flowsContainer;
-        _workflowSetter = CreateWorkflowSetter();
-        _stateSetter = CreateStateSetter();
-        
-        _next = CreateCallChain(flowsContainer.ServiceProvider);
-        
-        _registration = flowsContainer.FunctionRegistry.RegisterFunc<TParam, Unit>(
-            flowName,
-            PrepareAndRunFlow
-        );
-    }
-
-    private Next<TFlow, TParam, Unit> CreateCallChain(IServiceProvider serviceProvider)
-    {
-        return CallChain.Create<TFlow, TParam, Unit>(
-            _flowsContainer.Middlewares,
-            async (param, workflow) =>
+        var serviceProvider = FlowsContainer.ServiceProvider;
+        var workflowSetter = CreateWorkflowSetter();
+        var stateSetter = CreateStateSetter();
+        return CallChain.Create<TFlow, TParam, TResult>(
+            FlowsContainer.Middlewares,
+            runFlow: async (param, workflow) =>
             {
                 await using var scope = serviceProvider.CreateAsyncScope();
 
                 var flow = scope.ServiceProvider.GetRequiredService<TFlow>();
-                _workflowSetter(flow, workflow);
-                _stateSetter(flow, workflow.States);
-                                
-                await flow.Run(param);
+                workflowSetter(flow, workflow);
+                stateSetter(flow, workflow.States);
                 
-                return Unit.Instance;
+                var result = await runFlow(flow, param);
+                return result;
             }
+        );
+    }
+}
+
+public class Flows<TFlow> : BaseFlows<TFlow> where TFlow : Flow
+{
+    private readonly ParamlessRegistration _registration;
+    
+    public Flows(string flowName, FlowsContainer flowsContainer) : base(flowsContainer)
+    {
+        var callChain = CreateMiddlewareCallChain<Unit, Unit>(runFlow: async (flow, _) =>
+        {
+            await flow.Run();
+            return Unit.Instance;
+        });
+        
+        _registration = flowsContainer.FunctionRegistry.RegisterParamless(
+            flowName,
+            inner: workflow => callChain(Unit.Instance, workflow),
+            new Settings(routes: CreateRoutingInformation())
+        );
+    }
+
+    public async Task<ControlPanel?> ControlPanel(string instanceId)
+    {
+        var controlPanel = await _registration.ControlPanel(instanceId);
+        return controlPanel;
+    }
+    
+    protected Task<TState?> GetState<TState>(string functionInstanceId) where TState : WorkflowState, new() 
+        => _registration.GetState<TState>(functionInstanceId);
+    
+    public MessageWriter MessageWriter(string instanceId) 
+        => _registration.MessageWriters.For(instanceId);
+
+    public Task Run(string instanceId) 
+        => _registration.Invoke(instanceId);
+
+    public Task Schedule(string instanceId)
+        => _registration.Schedule(instanceId);
+    
+    public Task ScheduleAt(string instanceId, DateTime delayUntil) => _registration.ScheduleAt(instanceId, delayUntil);
+    public Task ScheduleIn(string functionInstanceId, TimeSpan delay) => _registration.ScheduleIn(functionInstanceId, delay);
+}
+
+public class Flows<TFlow, TParam> : BaseFlows<TFlow>
+    where TFlow : Flow<TParam>
+    where TParam : notnull
+{
+    private readonly FuncRegistration<TParam, Unit> _registration;
+    
+    public Flows(string flowName, FlowsContainer flowsContainer) : base(flowsContainer)
+    {
+        var callChain = CreateMiddlewareCallChain<TParam, Unit>(
+            runFlow: async (flow, param) =>
+            {
+                await flow.Run(param);
+                return Unit.Instance;
+            });
+        
+        _registration = flowsContainer.FunctionRegistry.RegisterFunc<TParam, Unit>(
+            flowName,
+            (param, workflow) => callChain(param, workflow)
         );
     }
 
@@ -214,97 +190,23 @@ public class Flows<TFlow, TParam>
         TParam param,
         TimeSpan delay
     ) => _registration.ScheduleIn(functionInstanceId, param, delay);
-    
-    private async Task<Result<Unit>> PrepareAndRunFlow(TParam param, Workflow workflow) 
-        => await _next(param, workflow);
-    
-    private Action<TFlow, States> CreateStateSetter()
-    {
-        var iHaveStateType = typeof(TFlow)
-            .GetInterfaces()
-            .SingleOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IHaveState<>));
-
-        if (iHaveStateType == null)
-            return (_, _) => { };
-
-        var stateType = iHaveStateType.GenericTypeArguments[0];
-        
-        //fetch state
-        var methodInfo = typeof(States)
-            .GetMethods()
-            .Single(m => m.Name == nameof(States.CreateOrGet) && !m.GetParameters().Any());
-
-        var genericMethodInfo = methodInfo.MakeGenericMethod(stateType);
-        var statePropertyInfo = iHaveStateType.GetProperty("State");
-        
-        return (flow, states) =>
-        {
-            var state = genericMethodInfo.Invoke(states, parameters: null);
-            statePropertyInfo!.SetValue(flow, state);
-        };
-    }
-    
-    private Action<TFlow, Workflow> CreateWorkflowSetter()
-    {
-        ParameterExpression flowParam = Expression.Parameter(typeof(TFlow), "flow");
-        ParameterExpression contextParam = Expression.Parameter(typeof(Workflow), "workflow");
-        MemberExpression propertyExpr = Expression.Property(flowParam, nameof(Flow<TParam, Unit>.Workflow));
-                
-        BinaryExpression assignExpr = Expression.Assign(propertyExpr, contextParam);
-
-        // Create a lambda expression
-        Expression<Action<TFlow, Workflow>> lambdaExpr = Expression.Lambda<Action<TFlow, Workflow>>(
-            assignExpr,
-            flowParam,
-            contextParam
-        );
-
-        // Compile and invoke the lambda expression
-        var setter = lambdaExpr.Compile();
-        return setter;
-    }
 }
 
-public class Flows<TFlow, TParam, TResult> 
+public class Flows<TFlow, TParam, TResult> : BaseFlows<TFlow>
     where TFlow : Flow<TParam, TResult>
     where TParam : notnull
 {
-    private readonly FlowsContainer _flowsContainer;
     private readonly FuncRegistration<TParam, TResult> _registration;
-
-    private readonly Next<TFlow, TParam, TResult> _next;
     
-    private readonly Action<TFlow, Workflow> _workflowSetter;
-    private readonly Action<TFlow, States> _stateSetter; 
-    
-    public Flows(string flowName, FlowsContainer flowsContainer)
+    public Flows(string flowName, FlowsContainer flowsContainer) : base(flowsContainer)
     {
-        _flowsContainer = flowsContainer;
-        _workflowSetter = CreateWorkflowSetter();
-        _stateSetter = CreateStateSetter();
-        _next = CreateCallChain(flowsContainer.ServiceProvider);
+        var callChain = CreateMiddlewareCallChain<TParam, TResult>(
+            runFlow: (flow, param) => flow.Run(param)
+        );
         
         _registration = flowsContainer.FunctionRegistry.RegisterFunc<TParam, TResult>(
             flowName,
-            (param, workflow) => PrepareAndRunFlow(param, workflow)
-        );
-    }
-
-    private Next<TFlow, TParam, TResult> CreateCallChain(IServiceProvider serviceProvider)
-    {
-        return CallChain.Create<TFlow, TParam, TResult>(
-            _flowsContainer.Middlewares,
-            runFlow: async (param, workflow) =>
-            {
-                await using var scope = serviceProvider.CreateAsyncScope();
-
-                var flow = scope.ServiceProvider.GetRequiredService<TFlow>();
-                _workflowSetter(flow, workflow);
-                _stateSetter(flow, workflow.States);
-                
-                var result = await flow.Run(param);
-                return result;
-            }
+            (param, workflow) => callChain(param, workflow)
         );
     }
 
@@ -333,53 +235,4 @@ public class Flows<TFlow, TParam, TResult>
 
     protected Task<TState?> GetState<TState>(string functionInstanceId) where TState : WorkflowState, new() 
         => _registration.GetState<TState>(functionInstanceId);
-    
-    private async Task<Result<TResult>> PrepareAndRunFlow(TParam param, Workflow workflow) 
-        => await _next(param, workflow);
-    
-    private Action<TFlow, Workflow> CreateWorkflowSetter()
-    {
-        ParameterExpression flowParam = Expression.Parameter(typeof(TFlow), "flow");
-        ParameterExpression contextParam = Expression.Parameter(typeof(Workflow), "workflow");
-        MemberExpression propertyExpr = Expression.Property(flowParam, nameof(Flow<TParam, Unit>.Workflow));
-                
-        BinaryExpression assignExpr = Expression.Assign(propertyExpr, contextParam);
-
-        // Create a lambda expression
-        Expression<Action<TFlow, Workflow>> lambdaExpr = Expression.Lambda<Action<TFlow, Workflow>>(
-            assignExpr,
-            flowParam,
-            contextParam
-        );
-
-        // Compile and invoke the lambda expression
-        var setter = lambdaExpr.Compile();
-        return setter;
-    }
-    
-    private Action<TFlow, States> CreateStateSetter()
-    {
-        var iHaveStateType = typeof(TFlow)
-            .GetInterfaces()
-            .SingleOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IHaveState<>));
-
-        if (iHaveStateType == null)
-            return (_, _) => { };
-
-        var stateType = iHaveStateType.GenericTypeArguments[0];
-        
-        //fetch state
-        var methodInfo = typeof(States)
-            .GetMethods()
-            .Single(m => m.Name == nameof(States.CreateOrGet) && !m.GetParameters().Any());
-
-        var genericMethodInfo = methodInfo.MakeGenericMethod(stateType);
-        var statePropertyInfo = iHaveStateType.GetProperty("State");
-        
-        return (flow, states) =>
-        {
-            var state = genericMethodInfo.Invoke(states, parameters: null);
-            statePropertyInfo!.SetValue(flow, state);
-        };
-    }
 }
