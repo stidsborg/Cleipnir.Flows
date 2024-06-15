68c91ff73488a07ecd99d7ada74605f7db141b03
stidsborg <stidsborg@cleipnir.net>
2024-06-15 07:25:29 +0200

    Moved shared functionality into BaseFlow type

--- Cleipnir.Flows/Flow.cs
+++ Cleipnir.Flows/Flow.cs
@@ -6,59 +6,34 @@ using Cleipnir.ResilientFunctions.Messaging;
 
 namespace Cleipnir.Flows;
 
-public abstract class Flow 
+public abstract class BaseFlow
 {
     public Workflow Workflow { get; init; } = null!;
     public Utilities Utilities => Workflow.Utilities;
     public Messages Messages => Workflow.Messages;
     public Effect Effect => Workflow.Effect;
+    
+    public Task<T> Capture<T>(string id, Func<Task<T>> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
+        => Effect.Capture(id, work, resiliencyLevel);
+    public Task<T> Capture<T>(string id, Func<T> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
+        => Effect.Capture(id, work, resiliencyLevel);
+    public Task Capture(string id, Func<Task> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
+        => Effect.Capture(id, work, resiliencyLevel);
+    public Task Capture(string id, Action work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
+        => Effect.Capture(id, work, resiliencyLevel);
+}
 
+public abstract class Flow : BaseFlow
+{
     public abstract Task Run();
-    
-    public Task<T> Capture<T>(string id, Func<Task<T>> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
-    public Task<T> Capture<T>(string id, Func<T> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
-    public Task Capture(string id, Func<Task> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
-    public Task Capture(string id, Action work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
 }
 
-public abstract class Flow<TParam> where TParam : notnull 
+public abstract class Flow<TParam> : BaseFlow where TParam : notnull 
 {
-    public Workflow Workflow { get; init; } = null!;
-    public Utilities Utilities => Workflow.Utilities;
-    public Messages Messages => Workflow.Messages;
-    public Effect Effect => Workflow.Effect;
-
     public abstract Task Run(TParam param);
-    
-    public Task<T> Capture<T>(string id, Func<Task<T>> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
-    public Task<T> Capture<T>(string id, Func<T> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
-    public Task Capture(string id, Func<Task> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
-    public Task Capture(string id, Action work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
 }
 
-public abstract class Flow<TParam, TResult> where TParam : notnull
+public abstract class Flow<TParam, TResult> : BaseFlow where TParam : notnull
 {
-    public Workflow Workflow { get; init; } = null!;
-    public Utilities Utilities => Workflow.Utilities;
-    public Messages Messages => Workflow.Messages;
-    public Effect Effect => Workflow.Effect;
-
     public abstract Task<TResult> Run(TParam param);
-    
-    public Task<T> Capture<T>(string id, Func<Task<T>> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
-    public Task<T> Capture<T>(string id, Func<T> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
-    public Task Capture(string id, Func<Task> work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
-    public Task Capture(string id, Action work, ResiliencyLevel resiliencyLevel = ResiliencyLevel.AtLeastOnce) 
-        => Effect.Capture(id, work, resiliencyLevel);
 }
