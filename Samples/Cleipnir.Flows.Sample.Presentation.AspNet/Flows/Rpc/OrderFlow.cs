b59a9c790fb2592ff29286b793cc4b54a5047084
stidsborg <stidsborg@cleipnir.net>
2024-10-06 08:37:42 +0200

    Added Polly to presentation sample

--- Samples/Cleipnir.Flows.Sample.Presentation.AspNet/Flows/Rpc/OrderFlow.cs
+++ Samples/Cleipnir.Flows.Sample.Presentation.AspNet/Flows/Rpc/OrderFlow.cs
@@ -1,4 +1,6 @@
 ﻿using Cleipnir.Flows.Sample.MicrosoftOpen.Clients;
+using Polly;
+using Polly.Retry;
 
 namespace Cleipnir.Flows.Sample.MicrosoftOpen.Flows.Rpc;
 
@@ -11,10 +13,26 @@ public class OrderFlow(
     public override async Task Run(Order order)
     {
         var transactionId = Guid.NewGuid();
-        
+
         await paymentProviderClient.Reserve(transactionId, order.CustomerId, order.TotalPrice);
         var trackAndTrace = await logisticsClient.ShipProducts(order.CustomerId, order.ProductIds);
         await paymentProviderClient.Capture(transactionId);
         await emailClient.SendOrderConfirmation(order.CustomerId, trackAndTrace, order.ProductIds);
     }
+
+    #region Polly
+
+    private ResiliencePipeline Pipeline { get; } = new ResiliencePipelineBuilder()
+        .AddRetry(
+            new RetryStrategyOptions
+            {
+                ShouldHandle = new PredicateBuilder().Handle<Exception>(),
+                BackoffType = DelayBackoffType.Exponential,
+                UseJitter = true, // Adds a random factor to the delay
+                MaxRetryAttempts = 10,
+                Delay = TimeSpan.FromSeconds(3),
+            }
+        ).Build();
+
+    #endregion
 }
