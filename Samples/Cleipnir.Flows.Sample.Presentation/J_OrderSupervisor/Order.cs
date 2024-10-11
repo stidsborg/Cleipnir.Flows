0384ca53267791ab9f2399287f0c4851113fbc39
stidsborg <stidsborg@cleipnir.net>
2024-10-11 12:11:27 +0200

    Implemented simple order supervisor flow example

---
+++ Samples/Cleipnir.Flows.Sample.Presentation/J_OrderSupervisor/Order.cs
@@ -0,0 +1,3 @@
+﻿namespace Cleipnir.Flows.Sample.Presentation.J_OrderSupervisor;
+
+public record Order(string OrderId, Guid CustomerId, IEnumerable<Guid> ProductIds, decimal TotalPrice);
---
+++ Samples/Cleipnir.Flows.Sample.Presentation/J_OrderSupervisor/OrderSupervisorFlow.cs
@@ -0,0 +1,25 @@
+﻿namespace Cleipnir.Flows.Sample.Presentation.J_OrderSupervisor;
+
+[GenerateFlows]
+public class OrderSupervisorFlow : Flow<Order>
+{
+    public override async Task Run(Order order)
+    {
+        try
+        {
+            await Message<FundsReserved>(TimeSpan.FromMinutes(5));
+        }
+        catch (TimeoutException)
+        {
+            await AlertBusinessOfStaleOrder(order);
+            await Message<FundsReserved>();
+        }
+        
+        //...
+    }
+
+    private Task AlertBusinessOfStaleOrder(Order order)
+    {
+        return Task.CompletedTask;
+    }
+}
