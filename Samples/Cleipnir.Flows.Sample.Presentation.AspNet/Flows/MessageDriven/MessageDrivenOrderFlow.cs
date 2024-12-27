5863fc02c394f83d058507d77ada153d76d4d096
stidsborg <stidsborg@cleipnir.net>
2024-12-27 08:59:16 +0100

    Improved MessageDrivenOrderFlow sample

--- Samples/Cleipnir.Flows.Sample.Presentation.AspNet/Flows/MessageDriven/MessageDrivenOrderFlow.cs
+++ Samples/Cleipnir.Flows.Sample.Presentation.AspNet/Flows/MessageDriven/MessageDrivenOrderFlow.cs
@@ -7,49 +7,38 @@ public class MessageDrivenOrderFlow(Bus bus) : Flow<Order>
 {
     public override async Task Run(Order order)
     {
-        //todo throw OrderProcessingException
         var transactionId = await Capture(Guid.NewGuid);
 
         await ReserveFunds(order, transactionId);
         
         await ShipProducts(order);
+        var trackAndTraceNumber = "";
         
         await CaptureFunds(order, transactionId);
-
-        var trackAndTraceNumber = "";
+        
         await SendOrderConfirmationEmail(order, trackAndTraceNumber);
     }
-
+    
+    private enum FailedAt
+    {
+        FundsReserved,
+        ProductsShipped,
+        FundsCaptured,
+        OrderConfirmationEmailSent,
+    }
+    
     #region MessagePublishers
-
-    private Task ReserveFunds(Order order, Guid transactionId)
-        => Capture(
-            () => bus.Send(new ReserveFunds(order.OrderId, order.TotalPrice, transactionId, order.CustomerId))
-        );
-    private Task CancelFundsReservation(Order order, Guid transactionId)
-        => Capture(
-            () => bus.Send(new CancelFundsReservation(order.OrderId, transactionId))
-        );
+    private Task ReserveFunds(Order order, Guid transactionId) 
+        => Capture(() => bus.Send(new ReserveFunds(order.OrderId, order.TotalPrice, transactionId, order.CustomerId)));
     private Task ShipProducts(Order order)
-        => Capture(
-            () => bus.Send(new ShipProducts(order.OrderId, order.CustomerId, order.ProductIds))
-        );
-    private Task CancelProductsShipment(Order order)
-        => Capture(
-            () => bus.Send(new CancelProductsShipment(order.OrderId))
-        );
+        => Capture(() => bus.Send(new ShipProducts(order.OrderId, order.CustomerId, order.ProductIds)));
     private Task CaptureFunds(Order order, Guid transactionId)
-        => Capture(
-            () => bus.Send(new CaptureFunds(order.OrderId, order.CustomerId, transactionId))
-        );
-    private Task ReverseTransaction(Order order, Guid transactionId)
-        => Capture(
-            () => bus.Send(new ReverseTransaction(order.OrderId, transactionId))
-        );
+        => Capture(() => bus.Send(new CaptureFunds(order.OrderId, order.CustomerId, transactionId)));
     private Task SendOrderConfirmationEmail(Order order, string trackAndTraceNumber)
-        => Capture(
-            () => bus.Send(new SendOrderConfirmationEmail(order.OrderId, order.CustomerId, trackAndTraceNumber))
-        );
-
+        => Capture(() => bus.Send(new SendOrderConfirmationEmail(order.OrderId, order.CustomerId, trackAndTraceNumber)));
+    private Task CancelProductsShipment(Order order)
+        => Capture(() => bus.Send(new CancelProductsShipment(order.OrderId)));
+    private Task CancelFundsReservation(Order order, Guid transactionId)
+        => Capture(() => bus.Send(new CancelFundsReservation(order.OrderId, transactionId)));
     #endregion
 }
--- Samples/Cleipnir.Flows.Sample.Presentation.AspNet/Flows/MessageDriven/Solution/MessageDrivenOrderFlow.cs
+++ Samples/Cleipnir.Flows.Sample.Presentation.AspNet/Flows/MessageDriven/Solution/MessageDrivenOrderFlow.cs
@@ -1,54 +1,72 @@
 ﻿using Cleipnir.Flows.Sample.MicrosoftOpen.Flows.MessageDriven.Other;
 using Cleipnir.ResilientFunctions.Reactive.Extensions;
+using Cleipnir.ResilientFunctions.Reactive.Utilities;
 
 namespace Cleipnir.Flows.Sample.MicrosoftOpen.Flows.MessageDriven.Solution;
 
 [GenerateFlows]
 public class MessageDrivenOrderFlow(Bus bus) : Flow<Order>
 {
-    public override async Task Run(Order order)
+   public override async Task Run(Order order)
     {
-        await Capture(() => Console.WriteLine("MessageDriven-OrderFlow Started"));
         var transactionId = await Effect.Capture(Guid.NewGuid);
 
         await ReserveFunds(order, transactionId);
-        var reservation = await EitherMessage<FundsReserved, FundsReservationFailed>();
-        if (reservation.HasSecond)
-            await Compensate(reservation.Second, order, transactionId);
-        
+        var reservation = await MessageOrTimeout<FundsReserved, FundsReservationFailed>(TimeSpan.FromSeconds(10));
+        if (!reservation.HasFirst)
+            await CleanUp(FailedAt.FundsReserved, order, transactionId);
+
         await ShipProducts(order);
-        var productsShipped = await EitherMessage<ProductsShipped, ProductsShipmentFailed>();
-        if (productsShipped.HasSecond)
-            await Compensate(productsShipped.Second, order, transactionId);
+        var productsShipped = await MessageOrTimeout<ProductsShipped, ProductsShipmentFailed>(TimeSpan.FromMinutes(5));
+        if (!productsShipped.HasFirst)
+            await CleanUp(FailedAt.ProductsShipped, order, transactionId);
+        var trackAndTraceNumber = productsShipped.First.TrackAndTraceNumber;
         
         await CaptureFunds(order, transactionId);
-        var capture = await EitherMessage<FundsCaptured, FundsCaptureFailed>();
-        if (capture.HasSecond)
-            await Compensate(capture.Second, order, transactionId);
+        var capture = await MessageOrTimeout<FundsCaptured, FundsCaptureFailed>(TimeSpan.FromSeconds(10));
+        if (!capture.HasFirst)
+            await CleanUp(FailedAt.FundsCaptured, order, transactionId);
         
-        await SendOrderConfirmationEmail(order, productsShipped.First);
-        var emailSent = await EitherMessage<OrderConfirmationEmailSent, OrderConfirmationEmailSent>();
-        if (emailSent.HasSecond)
-            await Compensate(emailSent.Second, order, transactionId);
-        
-        await Capture(() => Console.WriteLine("MessageDriven-OrderFlow Completed"));
+        await SendOrderConfirmationEmail(order, trackAndTraceNumber);
+        await MessageOrTimeout<OrderConfirmationEmailSent, OrderConfirmationEmailFailed>(TimeSpan.FromSeconds(10));
+    }
+    
+    private Task<EitherOrNone<T1, T2>> MessageOrTimeout<T1, T2>(TimeSpan timeout) =>
+        Messages
+            .TakeUntilTimeout(timeout)
+            .OfTypes<T1, T2>()
+            .FirstOrNone();
+
+    private async Task CleanUp(FailedAt failedAt, Order order, Guid transactionId)
+    {
+        switch (failedAt) 
+        {
+            case FailedAt.FundsReserved:
+                await CancelFundsReservation(order, transactionId);
+                break;
+            case FailedAt.ProductsShipped:
+                await CancelFundsReservation(order, transactionId);
+                await CancelProductsShipment(order);
+                break;
+            case FailedAt.FundsCaptured:
+                await CancelProductsShipment(order);
+                break;
+            case FailedAt.OrderConfirmationEmailSent:
+                //we accept this failure without cleaning up
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(failedAt), failedAt, null);
+        }
+
+        throw new OrderProcessingException($"Order processing failed at: '{failedAt}'");
     }
 
-    private async Task Compensate(object failureMessage, Order order, Guid transactionId)
+    private enum FailedAt
     {
-        var messages = 
-            (await Messages.TakeUntil(msg => msg == failureMessage).ToList())
-            .Append(failureMessage)
-            .ToList();
-
-        if (messages.Any(msg => msg is OrderConfirmationEmailFailed))
-            await CancelProductsShipment(order);
-        if (messages.Any(msg => msg is FundsCaptureFailed))
-            await ReserveFunds(order, transactionId);
-        else
-            await CancelFundsReservation(order, transactionId);
-
-        throw new Exception("Flow failed");
+        FundsReserved,
+        ProductsShipped,
+        FundsCaptured,
+        OrderConfirmationEmailSent,
     }
     
     private Task ReserveFunds(Order order, Guid transactionId) 
@@ -57,8 +75,8 @@ public class MessageDrivenOrderFlow(Bus bus) : Flow<Order>
         => Capture(() => bus.Send(new ShipProducts(order.OrderId, order.CustomerId, order.ProductIds)));
     private Task CaptureFunds(Order order, Guid transactionId)
         => Capture(() => bus.Send(new CaptureFunds(order.OrderId, order.CustomerId, transactionId)));
-    private Task SendOrderConfirmationEmail(Order order, ProductsShipped productsShipped)
-        => Capture(() => bus.Send(new SendOrderConfirmationEmail(order.OrderId, order.CustomerId, productsShipped.TrackAndTraceNumber)));
+    private Task SendOrderConfirmationEmail(Order order, string trackAndTraceNumber)
+        => Capture(() => bus.Send(new SendOrderConfirmationEmail(order.OrderId, order.CustomerId, trackAndTraceNumber)));
     private Task CancelProductsShipment(Order order)
         => Capture(() => bus.Send(new CancelProductsShipment(order.OrderId)));
     private Task CancelFundsReservation(Order order, Guid transactionId)
