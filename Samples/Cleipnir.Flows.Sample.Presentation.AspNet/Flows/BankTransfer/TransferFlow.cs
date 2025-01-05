60027b6dc568821a7fed1aa7b077b0890864bf85
stidsborg <stidsborg@cleipnir.net>
2025-01-05 09:27:42 +0100

    Simplified TransferFlow sample

--- Samples/Cleipnir.Flows.Sample.Presentation.AspNet/Flows/BankTransfer/TransferFlow.cs
+++ Samples/Cleipnir.Flows.Sample.Presentation.AspNet/Flows/BankTransfer/TransferFlow.cs
@@ -3,21 +3,21 @@
 namespace Cleipnir.Flows.Sample.MicrosoftOpen.Flows.BankTransfer;
 
 [GenerateFlows]
-public class TransferFlow(IBankCentralClient bankCentralClient) : Flow<Transfer>
+public class TransferFlow : Flow<Transfer>
 {
     public override async Task Run(Transfer transfer)
     {
-        var availableFunds = await bankCentralClient.GetAvailableFunds(transfer.FromAccount);
+        var availableFunds = await _bankCentralClient.GetAvailableFunds(transfer.FromAccount);
         if (availableFunds <= transfer.Amount)
             throw new InvalidOperationException("Insufficient funds on from account");
 
-        await bankCentralClient.PostTransaction(
+        await _bankCentralClient.PostTransaction(
             transfer.TransactionId,
             transfer.FromAccount,
             -transfer.Amount
         );
 
-        await bankCentralClient.PostTransaction(
+        await _bankCentralClient.PostTransaction(
             transfer.TransactionId,
             transfer.ToAccount,
             transfer.Amount
@@ -26,4 +26,6 @@ public class TransferFlow(IBankCentralClient bankCentralClient) : Flow<Transfer>
 
     private DistributedSemaphore DistributedLock(string account)
         => Workflow.Semaphores.Create("BankTransfer", account, maximumCount: 1);
+
+    private readonly IBankCentralClient _bankCentralClient = new BankCentralClient();
 }
